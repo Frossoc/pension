d66effb54b7bf05b4460f433626809483f7c1a07
Fernando Rosso <78184469+Frossoc@users.noreply.github.com>
2024-06-01 17:23:09 +0200

    act 3

--- packages/nextjs/app/debug/page.tsx
+++ packages/nextjs/app/debug/page.tsx
@@ -1,17 +1,33 @@
+import React from "react";
+import Image from "next/image";
 import { DebugContracts } from "./_components/DebugContracts";
 import type { NextPage } from "next";
-import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
-export const metadata = getMetadata({
-  title: "Debug Contracts",
-  description: "Debug your deployed 🏗 Scaffold-ETH 2 contracts in an easy way",
-});
+// Importar el componente Image de Next.js
 
 const Debug: NextPage = () => {
   return (
-    <>
-      <DebugContracts />
+    <div>
+      {/* Fondo de imagen */}
+      <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+        <Image
+          src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGt2OXNiOXpiMHB0cmF1dDBkeGhkc29mbXdhNDVva2Ezdjl4MTAxNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/KGkKyQAOU4U8DltXCD/giphy.webp"
+          alt="Background Image"
+          layout="fill"
+          objectFit="cover"
+          className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+        />
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Empower your Future</h2>
+            <p className="mt-6 text-lg leading-8 text-gray-300">Decentralized Pension</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Contenido del Debug */}
       <div className="text-center mt-8 bg-secondary p-10">
+        <DebugContracts />
         <h1 className="text-4xl my-0">Debug Contracts</h1>
         <p className="text-neutral">
           You can debug & interact with your deployed contracts here.
@@ -21,7 +37,7 @@ const Debug: NextPage = () => {
           </code>{" "}
         </p>
       </div>
-    </>
+    </div>
   );
 };
 
