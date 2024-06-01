e494f8ba2396cc9b5729f6f6b643b3ba855b0882
Fernando Rosso <78184469+Frossoc@users.noreply.github.com>
2024-06-01 21:26:14 +0200

    act9

--- packages/nextjs/app/debug/page.tsx
+++ packages/nextjs/app/debug/page.tsx
@@ -10,6 +10,83 @@ export const metadata = getMetadata({
 const Debug: NextPage = () => {
   return (
     <>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-8 h-8 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Total Likes</div>
+          <div className="stat-value text-primary">25.6K</div>
+          <div className="stat-desc">21% more than last month</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-8 h-8 stroke-current"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
+            </svg>
+          </div>
+          <div className="stat-title">Page Views</div>
+          <div className="stat-value text-secondary">2.6M</div>
+          <div className="stat-desc">21% more than last month</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <div className="avatar online">
+              <div className="w-16 rounded-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </div>
+          </div>
+          <div className="stat-value">86%</div>
+          <div className="stat-title">Tasks done</div>
+          <div className="stat-desc text-secondary">31 tasks remaining</div>
+        </div>
+      </div>
+
+      <div className="stats bg-primary text-primary-content">
+        <div className="stat">
+          <div className="stat-title">Account balance</div>
+          <div className="stat-value">$89,400</div>
+          <div className="stat-actions">
+            <button className="btn btn-sm btn-success">Add funds</button>
+          </div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Current balance</div>
+          <div className="stat-value">$89,400</div>
+          <div className="stat-actions">
+            <button className="btn btn-sm">Withdrawal</button>
+            <button className="btn btn-sm">Deposit</button>
+          </div>
+        </div>
+      </div>
+
+      <ul className="steps">
+        <li className="step step-info">Fly to moon</li>
+        <li className="step step-info">Shrink the moon</li>
+        <li className="step step-info">Grab the moon</li>
+        <li className="step step-error" data-content="?">
+          Sit on toilet
+        </li>
+      </ul>
+
       <DebugContracts />
       <div className="text-center mt-8 bg-secondary p-10">
         <h1 className="text-4xl my-0">Pension decentralized</h1>
